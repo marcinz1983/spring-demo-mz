40cdccab8686768138069ed2d442c70bc669ff7a
maniek <mpastuszka@gmail.com>
2021-02-20 12:41:28 +0100

    [change] - logger for controller

--- src/main/java/pl/sda/javalondek4springdemo/controller/MyFirstRestController.java
+++ src/main/java/pl/sda/javalondek4springdemo/controller/MyFirstRestController.java
@@ -1,14 +1,28 @@
 package pl.sda.javalondek4springdemo.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyFirstRestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyFirstRestController.class);
+
     @GetMapping("/say-something")
     public String saySomething() {
 
+        // logging levels
+        // trace
+        // debug
+        // info <-- current level
+        // warn --
+        // error -- inside catch
+        logger.info("/say-something");
+        logger.debug("is it visible???");
+        logger.trace("and this??");
         return "hello rest:P";
     }
 }
