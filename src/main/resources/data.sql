7d94e26453de23511f0843bbf57f48c482ab74aa
maniek <mpastuszka@gmail.com>
2021-03-14 14:17:58 +0100

    [change] - initialization sql script created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO BOOKS (IMIE, SURNAME, TYTUŁ)
+VALUES ('Henryk', 'Sienkiewicz', 'Potop');
+
+INSERT INTO BOOKS (IMIE, SURNAME, TYTUŁ)
+VALUES ('Henryk', 'Sienkiewicz', 'Pan Wołodyjowski');
+
+INSERT INTO BOOKS (IMIE, SURNAME, TYTUŁ)
+VALUES ('Henryk', 'Sienkiewicz', 'Ogniem i mieczem');
