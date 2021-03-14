dfaf3aefd0d8132ef7626513afd420c8510c901c
maniek <mpastuszka@gmail.com>
2021-03-14 13:59:03 +0100

    [change] - BookCrudRepository used as repository object

--- src/main/java/pl/sda/javalondek4springdemo/repository/BookRepository.java
+++ src/main/java/pl/sda/javalondek4springdemo/repository/BookRepository.java
@@ -3,9 +3,9 @@ package pl.sda.javalondek4springdemo.repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pl.sda.javalondek4springdemo.model.Book;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -13,27 +13,30 @@ public class BookRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(BookRepository.class);
 
-    private List<Book> books;
+    private final BookCrudRepository bookCrudRepository;
 
-    public BookRepository() {
-        List<Book> someBooks = List.of(
-            new Book(1L, "Henryk", "Sienkiewicz", "Potop"),
-            new Book(2L, "Henryk", "Sienkiewicz", "Pan Wołodyjowski"),
-            new Book(3L, "Henryk", "Sienkiewicz", "Quo Vadis")
-        );
-
-        this.books = new ArrayList<>(someBooks);
-
-        logger.info("book repository initialized with books: {}", books);
+    public BookRepository(BookCrudRepository bookCrudRepository) {
+        this.bookCrudRepository = bookCrudRepository;
     }
-
 
     public List<Book> findAllBooks() {
+//        var result = new ArrayList<Book>();
+//        bookCrudRepository.findAll()
+//                .forEach(book -> result.add(book));
 
-        return books;
+        var result = bookCrudRepository.findAllBooks();
+        logger.info("number of found books: [{}]", result.size());
+        logger.debug("found books: {}", result);
+        return result;
     }
 
+    @Transactional
     public boolean deleteBookWithId(Long id) {
-        return books.removeIf(book -> book.getId().equals(id));
+        boolean exists = bookCrudRepository.existsById(id);
+        if (exists) {
+            bookCrudRepository.deleteById(id);
+        }
+
+        return exists;
     }
 }
