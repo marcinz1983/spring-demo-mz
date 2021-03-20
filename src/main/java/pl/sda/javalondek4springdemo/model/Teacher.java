4ba30251546d3c34daabccb9554d2f0b8f8d2d6f
maniek <mpastuszka@gmail.com>
2021-03-20 13:24:31 +0100

    [change] - @Table annotations added

--- src/main/java/pl/sda/javalondek4springdemo/model/Teacher.java
+++ src/main/java/pl/sda/javalondek4springdemo/model/Teacher.java
@@ -7,9 +7,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
 @Entity
+@Table(name = "TEACHERS")
 public class Teacher {
 
     @Id
