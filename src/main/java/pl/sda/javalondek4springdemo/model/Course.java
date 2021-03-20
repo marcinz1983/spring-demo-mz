358c4fb8f21fcc50d04865dcc598f0a2f269a378
maniek <mpastuszka@gmail.com>
2021-03-20 11:42:58 +0100

    [add] - entities relations in progress...

---
+++ src/main/java/pl/sda/javalondek4springdemo/model/Course.java
@@ -0,0 +1,48 @@
+package pl.sda.javalondek4springdemo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String courseName;
+
+    @ManyToOne
+    private Teacher teacher;
+
+    public Course() {
+        // empty
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+}
---
+++ src/main/java/pl/sda/javalondek4springdemo/model/Teacher.java
@@ -0,0 +1,53 @@
+package pl.sda.javalondek4springdemo.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Basic
+    private UUID id;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany
+    private List<Course> courseList;
+
+    public Teacher() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
+    }
+}
