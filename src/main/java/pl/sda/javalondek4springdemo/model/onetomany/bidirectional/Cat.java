60db66d7eabfff989c7474f063b564fe47a06076
maniek <mpastuszka@gmail.com>
2021-03-21 13:46:45 +0100

    [add] - one to many mappings bidirectional created

---
+++ src/main/java/pl/sda/javalondek4springdemo/model/onetomany/bidirectional/Cat.java
@@ -0,0 +1,45 @@
+package pl.sda.javalondek4springdemo.model.onetomany.bidirectional;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity(name = "CatsManyToOne")
+@Table(name = "CATS_BELONGING_TO_OWNER")
+// Columns in db:
+// ID, NAME, MY_OWNER_ID
+public class Cat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "MY_OWNER_ID", referencedColumnName = "IDENTIFIER")
+    private Owner owner;
+
+    public Cat() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/pl/sda/javalondek4springdemo/model/onetomany/bidirectional/Owner.java
@@ -0,0 +1,63 @@
+package pl.sda.javalondek4springdemo.model.onetomany.bidirectional;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+// Columns in db:
+// ID, FIRST_NAME, LAST_NAME, CAT_ID
+@Entity(name = "OwnersWithManyCats")
+@Table(name = "OWNERS_WITH_MANY_CATS")
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long identifier;
+
+    private String firstName;
+
+    private String lastName;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "owner")
+    private List<Cat> cat;
+
+    public Owner() {
+    }
+
+    public Long getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(Long id) {
+        this.identifier = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Cat> getCat() {
+        return cat;
+    }
+
+    public void setCat(List<Cat> cat) {
+        this.cat = cat;
+    }
+}
