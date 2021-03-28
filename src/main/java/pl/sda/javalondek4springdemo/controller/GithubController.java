2a16d746d4be5c903a9371c3821156c1c7c9a56a
maniek <mpastuszka@gmail.com>
2021-03-28 12:36:36 +0200

    [change] - collection in rest template - not working at the moment:)

--- src/main/java/pl/sda/javalondek4springdemo/controller/GithubController.java
+++ src/main/java/pl/sda/javalondek4springdemo/controller/GithubController.java
@@ -3,7 +3,10 @@ package pl.sda.javalondek4springdemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.javalondek4springdemo.dto.UserRepoDto;
 import pl.sda.javalondek4springdemo.service.github.GithubRestService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/github")
@@ -18,5 +21,15 @@ public class GithubController {
     @GetMapping("/my-repos-as-string")
     String myRespos() {
         return githubRestService.findMyReposAsString();
+    }
+
+    @GetMapping("/my-repos")
+    List<UserRepoDto> myReposAsCollection() {
+        return githubRestService.findInfoOfMyRepos();
+    }
+
+    @GetMapping("/my-repos-array")
+    UserRepoDto[] myReposAsArray() {
+        return githubRestService.findInfoOfMyReposAsArray();
     }
 }
--- src/main/java/pl/sda/javalondek4springdemo/service/github/GithubRestService.java
+++ src/main/java/pl/sda/javalondek4springdemo/service/github/GithubRestService.java
@@ -1,11 +1,16 @@
 package pl.sda.javalondek4springdemo.service.github;
 
+import org.apache.catalina.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import pl.sda.javalondek4springdemo.dto.UserRepoDto;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -32,5 +37,30 @@ public class GithubRestService {
         return restTemplate.getForObject(allRepositoriesUrl, String.class, gitHubUser);
 //        Map<String, String> parametersNamesOverValues = Map.of("user", gitHubUser);
 //        return restTemplate.getForObject(allRepositoriesUrl, String.class, parametersNamesOverValues);
+    }
+
+    public List<UserRepoDto> findInfoOfMyRepos() {
+
+        ParameterizedTypeReference<List<UserRepoDto>> result = restTemplate.getForObject(allRepositoriesUrl,
+            new ParameterizedTypeReference<List<UserRepoDto>>() {}.getClass(),
+            gitHubUser);
+
+        return  result;
+    }
+
+    public UserRepoDto[] findInfoOfMyReposAsArray() {
+        return restTemplate.getForObject(allRepositoriesUrl, UserRepoDto[].class, gitHubUser);
+    }
+
+//    private List<String> doNothing(List<String> strings) {
+//        return strings;
+//    }
+//
+//    private List<UserRepoDto> doNothing(List<UserRepoDto> strings) {
+//        return strings;
+//    }
+    // this is how JVM see those methods
+        private List doNothing(List strings) {
+        return strings;
     }
 }
